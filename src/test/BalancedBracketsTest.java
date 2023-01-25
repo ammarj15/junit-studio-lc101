5a9e8d26e54e75e8963abe6de3937ec8c7e260e9
Ammar <ammarj15@gmail.com>
2023-01-25 11:42:00 -0600

    some java tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,45 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
-
+import static org.junit.Assert.fail;
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void hasEqualNumOfBothBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][[[]]][][]"));
+    }
 
+    @Test(expected = AssertionError.class)
+    public void returnsFalseWhenUntrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][][[]"));
+        fail("Should not reach here");
+    }
+
+    @Test
+    public void returnsTrueWhenNoInput() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void inputButWithNoBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Ammar was (here)"));
+    }
+
+    @Test(expected = AssertionError.class)
+    public void onlyPositives() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[["));
+    }
+    @Test(expected = AssertionError.class)
+    public void onlyNegatives() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("]]]]"));
+    }
 }
